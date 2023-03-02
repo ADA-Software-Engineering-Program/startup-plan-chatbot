b6186ca0f3146c95d9a7c99b29c7a1556d437337
chisomruth <chisomruth1212@gmail.com>
2023-03-02 17:54:58 +0100

    new changes

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template, request, jsonify
 from user_chat import get_response
 app = Flask(__name__)
 
-@app.get("/")
+@app.route("/", methods = ["GET"])
 def index_get():
     return render_template("base.html")
 
-@app.post("/predict")
+@app.route("/predict", methods = ["POST"])
 def predict():
     text = request.get_json().get("message")
     
