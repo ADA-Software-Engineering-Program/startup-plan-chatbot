b738cb95ec94a376f5c05a82797272bb643eeb5b
chisomruth <chisomruth1212@gmail.com>
2023-03-07 15:13:03 +0100

    new

--- user_app.py
+++ user_app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, jsonify
-
+from flask_cors import CORS
 from user_chat import get_response
-app = Flask(__name__)
 
-@app.route("/", methods=["get"])
-def index_get():
-    return render_template("base.html")
+app = Flask(__name__)
+CORS(app)
 
 @app.route("/predict", methods=["post"])
 def preict():
