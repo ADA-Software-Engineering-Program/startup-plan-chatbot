0ec880cba93d00ede9adf8cfb192030b4953baf0
chisomruth <chisomruth1212@gmail.com>
2023-03-09 21:49:55 +0100

    new changes

--- user_app.py
+++ user_app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 from user_chat import get_response
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=["get"])
 def index_get():
