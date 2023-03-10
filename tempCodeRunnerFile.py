d38eaca7d5495d7703a7fe98ecf8c8a4cef8762e
chisomruth <chisomruth1212@gmail.com>
2023-03-10 12:24:24 +0100

    new

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -12,7 +12,7 @@ def index_get():
     return render_template("base.html")
 
 @app.route("/predict", methods=["post"])
-def preict():
+def predict():
     text = request.get_json().get("message")
     
     response =get_response(text)
